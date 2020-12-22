767de49a5693b21d7f3c9251ca117835be5153b4
EdgarVera80 <edgar.vera80@gmail.com>
2020-12-22 09:43:26 -0600

    Se cambia la estructura

---
+++ loop_etiquetas.sql
@@ -0,0 +1,27 @@
+SET SERVEROUTPUT ON
+DECLARE
+    s PLS_INTEGER :=0;
+    i PLS_INTEGER :=0;
+    j PLS_INTEGER;
+    
+BEGIN
+    <<parent>>
+    LOOP
+        --Print Parent
+        i := i + 1;
+        j := 100;
+        DBMS_OUTPUT.PUT_LINE('Parent : ' || i);
+        
+        <<child>>
+        LOOP
+            --Print Child
+            EXIT parent WHEN (i > 3);
+            DBMS_OUTPUT.PUT_LINE('Child : ' || j);
+            j := j + 1;
+            EXIT child WHEN (j > 105);
+       END LOOP child;
+    END LOOP parent;
+    
+    DBMS_OUTPUT.put_line('FINISH!!!!!1');
+        
+END;
